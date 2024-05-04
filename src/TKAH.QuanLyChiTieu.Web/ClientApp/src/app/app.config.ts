7094bf05054a15a6947ee51e660477ea9ded1396
ximvhs <ximvhs26092002@gmail.com>
2024-05-04 17:51:10 +0700

    c

--- src/TKAH.QuanLyChiTieu.Web/ClientApp/src/app/app.config.ts
+++ src/TKAH.QuanLyChiTieu.Web/ClientApp/src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNoopAnimations(),provideEnvironmentNgxMask(),]
+  providers: [provideRouter(routes), provideNoopAnimations(),provideEnvironmentNgxMask(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(),]
 };
